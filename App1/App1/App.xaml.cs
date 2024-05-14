de7b492e4f0b9c56853d6d9f91d8a0f0984b203e
sergplesh <144843607+sergplesh@users.noreply.github.com>
2024-05-14 23:16:20 +0500

    Добавьте файлы проекта.

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            MainPage = new NavigationPage(new MainPage()); // Устанавливает MainPage в качестве первой страницы для навигации
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
